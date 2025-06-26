f7c4d8ae67f365d89864fd454c8c5f4f9b8c9e48
Daniel Gherghetta <gherghetta@gmail.com>
2025-06-26 14:55:29 +0200

    update workflow 4

--- .github/scripts/fetch-dlls.sh
+++ .github/scripts/fetch-dlls.sh
@@ -16,7 +16,7 @@ for REPO in $REPOS; do
     | jq -r '.assets[] | select(.name | endswith(".dll")) | .browser_download_url')
 
   if [[ -n "$ASSET_URL" ]]; then
-    curl -L -H "Authorization: token $TOKEN" "$ASSET_URL" -o "bots/${REPO}.dll"
+    curl -L -H "Authorization: token $TOKEN" "$ASSET_URL" -o "CompiledBots/${REPO}.dll"
   else
     echo "No DLL found for $REPO"
   fi
