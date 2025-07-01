1b997b15b62408c5845476536e791c861c7b9e35
Daniel Gherghetta <gherghetta@gmail.com>
2025-07-01 21:28:47 +0200

    update readme

--- src/TournamentRunner/Abstractions.cs
+++ src/TournamentRunner/Abstractions.cs
@@ -25,10 +25,11 @@ public class PokerAction : PokerEvent
     public int? Amount { get; set; } // Only for Raise
 }
 
+//Detta objektet kommer en gång per hand sist, och visar det totala resultatet av handen.
 public class HandResult : PokerEvent
 {
     //Card of the bot that had the SB
-    public Card SmallBlindBotCard { get; set; } = null!; 
+    public Card SmallBlindBotCard { get; set; } = null!;
     //Card of the bot that has the BB
     public Card BigBlindBotCard { get; set; } = null!;
     public Card CommunityCard { get; set; } = null!;
@@ -44,20 +45,29 @@ public class HandResult : PokerEvent
 
 public class GameState
 {
+    //Alltid *din* stack, hur många chips du har
     public int MyStack { get; set; }
+    //Alltid motståndarens stack, hur många chips den har
     public int OpponentStack { get; set; }
     public int Pot { get; set; }
+    //Det första kortet som man har från första början av handen
     public Card MyCard { get; set; } = null!;
+    //Det andra kortet som man ser först efter flop
     public Card? CommunityCard { get; set; }
+    //Hur mycket mer du behöver betta för att Call
     public int ToCall { get; set; }
+    //Hur mycket mer du behöver höja utöver ToCall för att höja
     public int MinRaise { get; set; }
     //The first action belongs to the bot that had the SB, if there is 0 objects 
     // in this list when the bot gets this objects, you have smallblind, 
     // ActionHistory.Count % 2 == 0 is small blind
     // ActionHistory.Count % 2 == 1 is big blind
     public List<PokerAction> ActionHistory { get; set; } = new();
+    // Detta resultat blir satt först efter en vinnare är utsedd i handen. 
     public HandResult? HandResult { get; set; }
 }
+
+
 
 public class Card
 {
